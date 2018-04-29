637748c4dfa678feb76647594f9b5270c5f265bc
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-29 20:07:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ return "Currently serving " + katzDeliLine.shift() + "."
 
 function currentLine(katzDeliLine){
 
+
+
+
 if (katzDeliLine.length === 0) {
   return "The line is currently empty."
 }
